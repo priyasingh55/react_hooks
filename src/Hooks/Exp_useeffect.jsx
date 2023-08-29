b6eeaebc2350262b70077d9b7dac28ea9a253286
priyasingh55 <138660538+priyasingh55@users.noreply.github.com>
2023-08-29 13:25:36 +0530

    1 file modified

--- src/Hooks/Exp_useeffect.jsx
+++ src/Hooks/Exp_useeffect.jsx
@@ -3,10 +3,16 @@ import React,{use,useState, useEffect} from "react";
 export default function Resource(){
   const[resourceType, setResourceType]=useState("posts");
   console.log("render")
-
+// this array resourcetype will be changed when it is render
   useEffect(()=>{
     console.log("resource type changed")
   },[resourceType])
+
+
+// this empty array gonna never be changes when state is changed
+  // useEffect(()=>{
+  //   console.log("onMount")
+  // },[])
 
 return(
   <>
