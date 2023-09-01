1d5d51e687aa99d176acdaf955e2b292d34c5aca
priyasingh55 <138660538+priyasingh55@users.noreply.github.com>
2023-09-01 11:49:47 +0530

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import Exp_useState from './Hooks/Exp_usestate';
 import './style.css';
 import Count from "./Hooks/Example_usestate";
 import Resource from "./Hooks/Exp_useeffect";
+import EXp_usestatememo from './Hooks/Exp_usestatememo';
 
 export default function App() {
   return (
     <div>
       
       {/* <Exp_useState /> */}
-      <Count />
+      {/* <Count /> */}
       {/* <Resource /> */}
+      <EXp_usestatememo />
       
     </div>
   );
