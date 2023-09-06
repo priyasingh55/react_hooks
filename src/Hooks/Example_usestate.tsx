ec7c910b82e72cf93fd6fe35749bf93b66be2670
priyasingh55 <138660538+priyasingh55@users.noreply.github.com>
2023-09-07 01:30:06 +0530

    1 file modified

--- src/Hooks/Example_usestate.tsx
+++ src/Hooks/Example_usestate.tsx
@@ -1,88 +1,42 @@
-import React,{use, useEffect, useState} from 'react';
-
-
-// function countInitial(){
-//   console.log("run function")
-//   return 5
-
-// }
-//  function Count(){
-// const [count,setCount]=useState(5);
-// ===============or=========
-  // const [count, setCount]=useState(()=>{
-  //   console.log("run function");
-  //   return 10;
-  // })
-  // ==============or=============
-  // it run every single time when function render
-  // const [count ,setCount]= useState(countInitial())
-
-  // it run single time
-  // const [count, setCount]= useState(()=>countInitial())
-
-
-
-  // function decrementCount(){
-    // setCount(count-1)
-    // setCount(prevCount => prevCount-1)
-    // setCount(prevCount => prevCount-1)
-  // }
-  // function incrementCount(){
-    // setCount(count+1)
-    // setCount(prevCount => prevCount+1)
-    // setCount(prevCount => prevCount+1)
-  // }
-
-
-
-// return(
-
-// <>
-// <button onClick={decrementCount}>-</button>
-// <span> &nbsp;{count}&nbsp; </span>
-// <button onClick={incrementCount}>+</button>
-// <button onClick={() =>{
-//   setCount(count*0)
-// }}>Reset</button>
-// </>
-// )
-// }
-// export default Count;
-
-// another way
+import React, { useState } from 'react'
 export default function Count(){
 
-  const [number, setNumber]= useState(1);
-  const [flag, setFlag] =useState(0)
-  
+  useState
+  const [name,setName ]=useState("");
 
-  useEffect(()=>{
-    console.log("mount")
-  },[])
-  useEffect(()=>{
-    console.log("update only for number")
-    return ()=>{
-      console.log("unmount")
+  const handleInputChange = (event) => {
+        setName(event.target.value);
+      };
 
-    }
-  },[number])
-
-
-  useEffect(()=>{
-    // console.log("update for all")
-  })
-
-  return(
-    
-    <>
-    <button onClick={()=>setNumber((currVal)=>{
-      return currVal+1
-
-    })}>+</button>
-    {number}
-    <button onClick={()=>setNumber(number-1)}>-</button> &nbsp;
-    <button onClick={()=>setFlag(flag+1)}>{flag}</button>
-    </>
+  return (
+    <div>
+    <button onClick={()=>setName((currVal)=>{
+   return currVal="Vishal"
+    })}>{name}</button>
+    &nbsp;
+    <input type="text"  value={name} onChange={handleInputChange}/>
+    </div>
   )
-
+    
 }
+// function MyComponent() {
+//   const [inputValue, setInputValue] = useState('');
+
+//   const handleInputChange = (event) => {
+//     setInputValue(event.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <label htmlFor="myInput">Input:</label>
+//       <input
+//         type="text"
+//         id="myInput"
+//         value={inputValue}
+//         onChange={handleInputChange}
+//       />
+//     </div>
+//   );
+// }
+
+// export default MyComponent;
