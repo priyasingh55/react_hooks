4e1feef0da664791901c220c454a754433eedf3b
priyasingh55 <138660538+priyasingh55@users.noreply.github.com>
2023-09-11 20:26:18 +0530

    1 file modified

--- src/samples/Todo _app.tsx
+++ src/samples/Todo _app.tsx
@@ -3,25 +3,65 @@ import React, { useState } from "react";
 
 export default function Todo_app(){
   const [name,setName]=useState("Apple1")
-  const [list,setList]=useState(["hello"])
+  const [text,setText]=useState(["PRIYA","AMRITA" ,"PIYUSH"])
+  const [list,setList]=useState(["hello","hiii","nope"])
+  
+  
   const [lastname,setLastname]=useState('empty')
   function updateList(){
     setLastname(name)
     setName("")
     setList([name,...list])
    }
+   function deleteFromList(element){
+    
+    const newList=list.filter((e)=>{
+      
+      return e !==element
+    })
+    console.log(newList)
+    setList([newList])
+    
+
+   }
+
+  const createLIst = (element)=>{
+    return (<li>{element} <button onClick={()=>deleteFromList(element)}>Delete</button></li>)
+  }
+  function emptyArray(){
+   const arr= []
+   console.log(arr)
+   
+
+
+  }
+  // console.log(emptyArray())
+
+  
 
   return(
     <div>
+      <button onClick={()=>emptyArray()}>{text}</button>
       <input type="text" value={name} onInput={e=>setName(e.target.value)}/>
-      <button onClick={e=>updateList()}> +</button>
+      <button onClick={updateList}> +</button>
     
       {name}
       &nbsp;
       {lastname}
       &nbsp;
-      {<ol>{list.map(element=><li>{element}</li>)}</ol>}
+      {<ol>{list.map (createLIst)
+       
+      }</ol>}
+      <button onClick={()=>setList([])}> Delete</button>
       
     </div>
   )
 }
+
+
+
+// const arr =[1,2,3,4,5,6,7,8,9];
+//  const newArr=arr.filter((currval)=>{
+//    return currval!==3
+// });
+// console.log(newArr)
